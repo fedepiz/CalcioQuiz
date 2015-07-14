ef8eb5fce7681260489260d70153edc0adc8efe0
Federico Pizzuti <pizzutifederico27@gmail.com>
2015-07-14 15:38:11 +0200

    Done first touch of Quiz Setup view

--- app/src/main/java/fedepiz/calcioquiz/main/Main.java
+++ app/src/main/java/fedepiz/calcioquiz/main/Main.java
@@ -14,33 +14,4 @@ import java.util.List;
  */
 public class Main {
 
-    private Resources resources;
-
-    public Main(Resources resources) {
-        this.resources = resources;
-    }
-
-
-    public  void run() {
-        try {
-            Log.i("CalcioQuiz", "Main launched");
-            List<Question> questionList = loadQuestions();
-            if (questionList.size() > 0) {
-            } else {
-                Log.i("CalcioQuiz","No question parsed");
-            }
-        } catch (Exception ex) {
-            Log.i("CalcioQuiz", "ERROR: " + ex.getMessage());
-        }
-        Log.i("CalcioQuiz","Application terminated successfully");
-    }
-
-
-    public List<Question> loadQuestions() throws QuestionParserException {
-        Log.i("CalcioQuiz", "Main launched");
-        InputStream ins = resources.openRawResource(R.raw.domande);
-        QuestionParser parser = new QuestionParser();
-        List<Question> questionList = parser.questionsFromXML(ins);
-        return questionList;
-    }
 }
--- app/src/main/java/fedepiz/calcioquiz/ui/QuizSetupActivity.java
+++ app/src/main/java/fedepiz/calcioquiz/ui/QuizSetupActivity.java
@@ -1,19 +1,33 @@
 package fedepiz.calcioquiz.ui;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import fedepiz.calcioquiz.R;
 
 
 public class QuizSetupActivity extends ActionBarActivity {
 
+    private TextView txtMinPoints;
+    private TextView txtMaxPoints;
+    private TextView txtNumQuestions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_setup);
+
+        this.txtMinPoints = (TextView)findViewById(R.id.txtMinPoints);
+        this.txtMaxPoints = (TextView)findViewById(R.id.txtMaxPoints);
+        this.txtNumQuestions = (TextView)findViewById(R.id.txtNumQuestions);
     }
 
     @Override
@@ -36,5 +50,38 @@ public class QuizSetupActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //UI BACKEND
+
+    private void launchGame() {
+        Integer minPoints = parseIntFromText(txtMinPoints.getText().toString(),0);
+        Integer maxPoints = parseIntFromText(txtMaxPoints.getText().toString(),1000);
+        Integer numQuestions = parseIntFromText(txtNumQuestions.getText().toString(),10);
+
+        Intent intent = new Intent(this,QuizActivity.class);
+        intent.putExtra("minPoints",minPoints);
+        intent.putExtra("maxPoints",maxPoints);
+        intent.putExtra("numQuestions",numQuestions);
+
+        Log.i("CalcioQuiz",minPoints.toString());
+        Log.i("CalcioQuiz",maxPoints.toString());
+        Log.i("CalcioQuiz",numQuestions.toString());
+        startActivity(intent);
+    }
+
+    //PROCESSING
+    private int parseIntFromText(String str,int defaultValue) {
+        if(str == "") {
+            return defaultValue;
+        } else {
+            return Integer.parseInt(str);
+        }
+    }
+
+    //UI FRONTEND
+
+    public void btnStartGameOnClick(View view) {
+        launchGame();
     }
 }
