c4dc50f0869be1fdff065d152645173d1144e1a8
Federico Pizzuti <pizzutifederico27@gmail.com>
2015-07-16 02:23:23 +0200

    Rimosso DataLoader. La sua funzionalita' e' stata spostata in GameData

--- app/src/main/java/fedepiz/calcioquiz/data/GameData.java
+++ app/src/main/java/fedepiz/calcioquiz/data/GameData.java
@@ -1,9 +1,12 @@
 package fedepiz.calcioquiz.data;
 
 import android.content.res.Resources;
+import android.util.Log;
 
+import java.io.InputStream;
 import java.util.List;
 
+import fedepiz.calcioquiz.R;
 import fedepiz.calcioquiz.model.Question;
 import fedepiz.calcioquiz.model.QuestionParser;
 import fedepiz.calcioquiz.model.QuestionParserException;
@@ -19,12 +22,23 @@ public class GameData {
         return questions;
     }
 
+
     public static void setQuestions(List<Question> questions) {
         GameData.questions = questions;
     }
 
     public static void loadGameData(Resources resources) throws Exception {
-        DataLoader dataLoader = new DataLoader(resources);
-        GameData.setQuestions(dataLoader.loadQuestions());
+        loadQuestions(resources);
     }
+
+    private static void loadQuestions(Resources resources) throws QuestionParserException{
+        Log.i("CalcioQuiz", "Main launched");
+        InputStream ins = resources.openRawResource(R.raw.domande);
+        QuestionParser parser = new QuestionParser();
+        List<Question> questionList = parser.questionsFromXML(ins);
+        setQuestions(questionList);
+    }
+
 }
+
+
